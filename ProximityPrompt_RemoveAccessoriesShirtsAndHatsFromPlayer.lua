3617d99a0b1b47128cb17177dd799da8ff6d02c9
vissequ <93226346+vissequ@users.noreply.github.com>
2022-03-17 13:41:56 -0700

    Create ProximityPrompt_RemoveAccessoriesShirtsAndHatsFromPlayer.lua

---
+++ ProximityPrompt_RemoveAccessoriesShirtsAndHatsFromPlayer.lua
@@ -0,0 +1,7 @@
+script.Parent.ProximityPrompt.Triggered:Connect(function(plr)
+	for i,v in pairs(plr.Character:GetChildren()) do
+		if v:IsA("Accessory") or v:IsA("Shirt") or v:IsA("Pants") then
+			v:Destroy()
+		end
+	end
+end)
