d681cd5358c2e1bc57eb54430db42a10545a8de3
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-04 09:37:27 -0700

    Update ProximityPrompt_RemoveAccessoriesShirtsAndHatsFromPlayer.lua

--- ProximityPrompt_RemoveAccessoriesShirtsAndHatsFromPlayer.lua
+++ ProximityPrompt_RemoveAccessoriesShirtsAndHatsFromPlayer.lua
@@ -1,3 +1,5 @@
+--When placed inside a ProximityPrompt activating that prompt will remove accessories, shirts, and pants.
+
 script.Parent.ProximityPrompt.Triggered:Connect(function(plr)
 	for i,v in pairs(plr.Character:GetChildren()) do
 		if v:IsA("Accessory") or v:IsA("Shirt") or v:IsA("Pants") then
