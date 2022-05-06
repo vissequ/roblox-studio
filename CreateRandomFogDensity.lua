e82c8b2007512ae2b51e34cc26dff4a50ee423f5
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-06 12:58:55 -0700

    Create CreateRandomFogDensity.lua

---
+++ CreateRandomFogDensity.lua
@@ -0,0 +1,33 @@
+local L = game:service("Lighting") 
+
+local function SetDensity(target)
+	local negOrPos = 1
+	if target > L.Atmosphere.Density then
+		negOrPos = 1
+	elseif target < L.Atmosphere.Density then
+		negOrPos = -1
+	end
+	for i = L.Atmosphere.Density, target, .01 * negOrPos do
+		L.Atmosphere.Density = i
+		wait(1)
+	end
+end
+
+local T_targetDensities = {
+	.21,
+	.22,
+	.23,
+	.24,
+	.25,
+	.26,
+	.27,
+	.28,
+	.29,
+	.3,
+}
+
+while wait(60*3) do
+  local targetDensity = T_targetDensities[math.random(1,#T_targetDensities)]
+  print("TargetDensity is now: "..targetDensity)
+  SetDensity(targetDensity)
+end
