de1e8ac18a208c77ba01abb93c3f66cbc124660f
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-07 18:17:08 -0700

    Create ObfuscationCheck.lua

---
+++ ObfuscationCheck.lua
@@ -0,0 +1,36 @@
+local shouldDestroy = true
+
+local SearchFor = {
+ "#",
+ "$",
+ "1",
+ "2",
+ "3",
+ "4",
+ "5",
+ "6",
+ "7",
+ "8",
+ "9",
+ "0",
+}
+
+local Selection = game:GetService("Selection")
+
+for _, object in pairs(Selection:Get()) do
+ for i,v in pairs(object:GetDescendants()) do
+  if v:IsA("Script") then
+   print("Script: "..v.Name)
+   for j,k in pairs(SearchFor) do
+    if string.find(v.Name, k) then
+     if shouldDestroy == false then
+      warn("SCRIPT: "..v.Name)
+     else
+      --v:Destroy() --this is commented out for your protection. uncomment it once you understand the script.
+      print("DESTROYED: "..v.Name)
+     end
+    end
+   end
+  end
+ end
+end
