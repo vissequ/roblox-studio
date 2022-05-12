5bf3d19fc9092f9cac0fb8433b1da5c5b05445c8
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-12 14:52:22 -0700

    Create Weather_EndTaslemRain.lua

---
+++ Weather_EndTaslemRain.lua
@@ -0,0 +1,25 @@
+--LocalScript
+ 
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+ 
+local player = Players.LocalPlayer
+local remoteEvent = ReplicatedStorage.REs:WaitForChild("WeatherClientEnd")
+local playerGui = player:WaitForChild("PlayerGui")
+ 
+local function weatherEndFired()
+	local RainVolume = playerGui.Rain.Volume
+	for i = RainVolume, 0, -.1 do
+		playerGui.Rain.Volume = i
+		wait(.1)
+	end
+	playerGui.Rain:Stop()
+	playerGui.RainyDay.Disabled = true
+	--workspace.SplashHome:Destroy()
+	local RainHome = workspace:FindFirstChild("Rain Home")
+	if RainHome then
+		RainHome:Destroy()
+	end
+end
+ 
+remoteEvent.OnClientEvent:Connect(weatherEndFired)
