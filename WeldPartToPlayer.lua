71f81f36c9a00a339d1497c5f2441debea034e5e
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-12 14:24:55 -0700

    Create WeldPartToPlayer.lua

---
+++ WeldPartToPlayer.lua
@@ -0,0 +1,13 @@
+game.Players.PlayerAdded:Connect(function(plr)
+	plr.CharacterAdded:Connect(function()
+		local newNecklace = script:WaitForChild("NeonNecklace"):Clone()
+		newNecklace.Parent = plr.Character
+		local weld = Instance.new("WeldConstraint")
+		weld.Parent = newNecklace
+		weld.Part0 = newNecklace
+		weld.Part1 = plr.Character.Head
+		newNecklace.CFrame = plr.Character.Head.CFrame + Vector3.new(0,-.5,0)
+		newNecklace.Orientation = Vector3.new(0, -46, -90)
+		newNecklace.Control.Disabled = false
+	end)
+end)
