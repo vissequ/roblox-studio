bf6a31629335c7c738be4d176905def13645f312
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-19 08:22:59 -0700

    Create Give Player Title Above Head.lua

---
+++ GUIs/Give Player Title Above Head.lua
@@ -0,0 +1,23 @@
+local tc = script.BillboardGui
+--billboard GUI must be a child of this script
+
+local Players = game:GetService("Players")
+
+game.Players.PlayerAdded:Connect(function(plr)
+	plr.CharacterAdded:Connect(function()
+		local h = plr.Character.Humanoid
+		h.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+		local bGUI = tc:Clone()
+		bGUI.PlayerName.Text = plr.Name
+		if plr.Name == "vissequ" then
+			bGUI.Title.Text = "CREATOR"
+		elseif plr.Name == "ponybruh" then
+			bGUI.Title.Text = "HEAD TESTER"
+		elseif plr:IsInGroup(4068345) then
+			bGUI.Title.Text = "VISSEQU STUDIOS"
+		else
+			bGUI.Title.Text = "LEVEL 1"
+		end
+		bGUI.Parent = plr.Character.Head
+	end)
+end)
