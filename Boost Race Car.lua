c130e23561003e71505731d686a7b296c5cde39a
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-19 08:32:00 -0700

    Create Boost Race Car.lua

---
+++ Boost Race Car.lua
@@ -0,0 +1,15 @@
+local debounce = false
+
+script.Parent.Touched:Connect(function(hit)
+	if not debounce then
+		debounce = true
+		if hit.Parent.Name == "RaceCar" then
+			local bv = Instance.new("BodyVelocity", hit.Parent.PrimaryPart)
+			bv.MaxForce = Vector3.new(math.huge,math.huge,math.huge)
+			bv.Velocity = hit.Parent.PrimaryPart.CFrame.LookVector * 100 + Vector3.new(0,20,0)
+			wait(2.3)
+			bv:Destroy()
+		end
+		debounce = false
+	end
+end)
