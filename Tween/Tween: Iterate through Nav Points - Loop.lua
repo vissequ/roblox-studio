24046d651db8b939e92b39e275f9ca8e7c81c2c5
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-20 10:58:12 -0700

    Update Tween: Iterate through Nav Points - Loop.lua

--- Tween/Tween: Iterate through Nav Points - Loop.lua
+++ Tween/Tween: Iterate through Nav Points - Loop.lua
@@ -1,3 +1,5 @@
+--This was programmed by vissequ#1301 (Discord). You may use this any way you wish, but please do not remove this credit.
+
 local planePoints = script.Parent.Parent
 local T_PlanePoints = planePoints:GetChildren()
 
