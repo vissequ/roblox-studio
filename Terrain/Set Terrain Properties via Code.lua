22015175899c6004b8b948780019f236ab9ab3fc
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-20 13:24:25 -0700

    Create Set Terrain Properties via Code.lua

---
+++ Terrain/Set Terrain Properties via Code.lua
@@ -0,0 +1,8 @@
+game.Workspace.Terrain:SetMaterialColor(Enum.Material.Sand,Color3.fromRGB(149, 131, 99))
+
+game.Workspace.Terrain.WaterColor = Color3.fromRGB(51, 82, 99)
+game.Workspace.Terrain.WaterTransparency = .3
+game.Workspace.Terrain.WaterWaveSize = .1
+game.Workspace.Terrain.WaterWaveSpeed = 7
+
+game.Lighting.FogEnd = 100000
