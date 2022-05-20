4f1513dfc75160186543a3390764ce4557b882f9
vissequ <93226346+vissequ@users.noreply.github.com>
2022-05-20 10:51:16 -0700

    Update DJ_Console_ServerScript.lua

--- DJ Console/DJ_Console_ServerScript.lua
+++ DJ Console/DJ_Console_ServerScript.lua
@@ -1,3 +1,5 @@
+--This was programmed by vissequ#1301 (Discord). You may use this any way you wish, but please do not remove this credit.
+
 local RS = game:GetService("ReplicatedStorage")
 local REs = RS:WaitForChild("REs")
 local rE = REs:WaitForChild("UpdateDJSongStatus")
