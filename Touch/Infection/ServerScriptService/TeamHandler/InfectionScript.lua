34edf34f27407f5f0816c674b309c4b5c84fa2e1
vissequ <93226346+vissequ@users.noreply.github.com>
2022-07-20 13:22:25 -0700

    Update InfectionScript.lua

--- Touch/Infection/ServerScriptService/TeamHandler/InfectionScript.lua
+++ Touch/Infection/ServerScriptService/TeamHandler/InfectionScript.lua
@@ -1,3 +1,4 @@
+--This was programmed by vissequ#1301 (Discord). You may use this any way you wish, but please do not remove this credit.
 -- must be placed inside the TeamHandler ModuleScript
 
 local sp = script.Parent
